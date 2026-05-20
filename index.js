17be1242d76ebffcd3af6929e3f0899980b465b6
UGO-ABATA <oye1934@gmail.com>
2026-05-20 15:43:16 +0100

    updated server

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const express = require ('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const router= require("./routes/todoRoutes");
-const live_url =  
-  "mongodb+srv://ZOM_30:Onyebuchi2012@cluster0.7nby8av.mongodb.net/userDB?appName=Cluster0";
+const live_url ="mongodb://Chizom:Ugonwaval@ac-cqxxdgy-shard-00-00.mtdpcdq.mongodb.net:27017,ac-cqxxdgy-shard-00-01.mtdpcdq.mongodb.net:27017,ac-cqxxdgy-shard-00-02.mtdpcdq.mongodb.net:27017/userDB?ssl=true&replicaSet=atlas-13jm5c-shard-0&authSource=admin&appName=Cluster0"
 const local_url = "mongodb://localhost:27017/userDB";
 
-mongoose
-  .connect(local_url) 
+mongoose.connect(live_url)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error("Connection Error: ", err));
 
